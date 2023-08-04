9f040372e58988e16790f9a661b839667bcc8eb0
Vishnu V V <ng.pingme@gmail.com>
2023-08-04 03:04:58 +0100

    Use Tile component and render some tiles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Lightning, Utils } from '@lightningjs/sdk'
+import Tile from './components/Tile'
 
 export default class App extends Lightning.Component {
   static getFonts() {
@@ -13,40 +14,44 @@ export default class App extends Lightning.Component {
         color: 0xfffbb03b,
         src: Utils.asset('images/background.png'),
       },
-      Logo: {
-        mountX: 0.5,
-        mountY: 1,
-        x: 960,
-        y: 600,
-        src: Utils.asset('images/logo.png'),
-      },
-      Text: {
-        mount: 0.5,
-        x: 960,
-        y: 720,
-        text: {
-          text: "Let's start Building!",
-          fontFace: 'Regular',
-          fontSize: 64,
-          textColor: 0xbbffffff,
+      Content: {
+        x: 100,
+        y: 100,
+        w: 1720,
+        h: 880,
+        rect: true,
+        color: 0x00000000,
+        Title: {
+          mount: 0.5,
+          x: 260,
+          text: {
+            text: 'Trending Movies This Week',
+            fontFace: 'Regular',
+            fontSize: 48,
+          },
+        },
+        MovieList: {
+          w: 800,
+          h: 350,
+          x: 380,
+          y: 270,
+          mount: 0.5,
         },
       },
     }
   }
 
   _init() {
-    this.tag('Background')
-      .animation({
-        duration: 15,
-        repeat: -1,
-        actions: [
-          {
-            t: '',
-            p: 'color',
-            v: { 0: { v: 0xfffbb03b }, 0.5: { v: 0xfff46730 }, 0.8: { v: 0xfffbb03b } },
-          },
-        ],
+    this.index = 0
+    this.dataLength = 6
+    const movieList = []
+    for (let i = 0; i < this.dataLength; i++) {
+      movieList.push({
+        type: Tile,
+        x: i * (300 + 30),
+        item: { label: `Train ${i + 1}`, src: `./images/sample${i + 1}.jpg` },
       })
-      .start()
+    }
+    this.tag('MovieList').children = movieList
   }
 }
