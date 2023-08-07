fb975af1640f93b7150377e9c835a9cbf420b63c
Vishnu V V <ng.pingme@gmail.com>
2023-08-08 00:51:48 +0100

    Create a formatter function to convert to collection of Row components

--- src/api/formatters/ItemFormatter.js
+++ src/api/formatters/ItemFormatter.js
@@ -1,8 +1,8 @@
 import Tile from '../../components/Tile'
+import Row from '../../components/Row'
 import { getImageUrl } from '../index'
 
 export function convertItemsToTiles(items = []) {
-  console.log('convertItemsToTiles', items)
   return items.map((item, index) => {
     return {
       type: Tile,
@@ -16,6 +16,21 @@ export function convertItemsToTiles(items = []) {
       w: 385,
       h: 545,
       x: index * 425,
+    }
+  })
+}
+
+export function convertItemsToRows(items = []) {
+  return items.map((item, index) => {
+    return {
+      type: Row,
+      item: {
+        title: item.title || item.name,
+        src: item.src || '',
+      },
+      w: 800,
+      h: 75,
+      y: index * 125,
     }
   })
 }
