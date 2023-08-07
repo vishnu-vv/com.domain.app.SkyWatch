b04ac27ab0a7a532120b893bc299e01678655cc6
Vishnu V V <ng.pingme@gmail.com>
2023-08-08 00:50:27 +0100

    Create a mock array for news list

---
+++ src/mock/news.js
@@ -0,0 +1,51 @@
+export const news = [
+  {
+    id: 1,
+    title: 'News 1',
+    description: 'News 1 description',
+    image: 'https://picsum.photos/200/300',
+    date: '2021-01-01',
+  },
+  {
+    id: 2,
+    title: 'News 2',
+    description: 'News 2 description',
+    image: 'https://picsum.photos/200/300',
+    date: '2021-01-02',
+  },
+  {
+    id: 3,
+    title: 'News 3',
+    description: 'News 3 description',
+    image: 'https://picsum.photos/200/300',
+    date: '2021-01-03',
+  },
+  {
+    id: 4,
+    title: 'News 4',
+    description: 'News 4 description',
+    image: 'https://picsum.photos/200/300',
+    date: '2021-01-04',
+  },
+  {
+    id: 5,
+    title: 'News 5',
+    description: 'News 5 description',
+    image: 'https://picsum.photos/200/300',
+    date: '2021-01-05',
+  },
+  {
+    id: 6,
+    title: 'News 6',
+    description: 'News 6 description',
+    image: 'https://picsum.photos/200/300',
+    date: '2021-01-06',
+  },
+  {
+    id: 7,
+    title: 'News 7',
+    description: 'News 7 description',
+    image: 'https://picsum.photos/200/300',
+    date: '2021-01-07',
+  },
+]
