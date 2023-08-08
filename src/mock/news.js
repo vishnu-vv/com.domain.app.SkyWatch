e634de586e8a9c30900ff747713d0feffb2cf550
Vishnu V V <ng.pingme@gmail.com>
2023-08-08 02:38:23 +0100

    Update the mock data with some genuine content

--- src/mock/news.js
+++ src/mock/news.js
@@ -1,51 +1,58 @@
 export const news = [
   {
     id: 1,
-    title: 'News 1',
-    description: 'News 1 description',
+    title: 'Scientists Make Breakthrough in Renewable Energy Generation',
+    description:
+      'A team of researchers has achieved a significant advancement in the field of renewable energy generation. Their new innovation promises to increase the efficiency of solar panels by up to 30%, making clean energy more accessible and cost-effective.',
     image: 'https://picsum.photos/200/300',
-    date: '2021-01-01',
+    date: '2023-08-08',
   },
   {
     id: 2,
-    title: 'News 2',
-    description: 'News 2 description',
+    title: 'Global Summit Addresses Climate Change Challenges',
+    description:
+      'World leaders and environmental experts gathered at a global summit to discuss urgent measures to combat climate change. The discussions focused on policy reforms, sustainable practices, and international cooperation to mitigate the growing environmental crisis.',
     image: 'https://picsum.photos/200/300',
-    date: '2021-01-02',
+    date: '2023-08-07',
   },
   {
     id: 3,
-    title: 'News 3',
-    description: 'News 3 description',
+    title: 'Tech Company Launches Innovative Health Tracking Device',
+    description:
+      'A leading technology company has unveiled a cutting-edge health tracking device that monitors various vital signs in real-time. This wearable device is set to revolutionize personal healthcare by providing users with continuous insights into their well-being.',
     image: 'https://picsum.photos/200/300',
     date: '2021-01-03',
   },
   {
     id: 4,
-    title: 'News 4',
-    description: 'News 4 description',
+    title: 'New Art Exhibition Showcases Diverse Creative Expressions',
+    description:
+      "An upcoming art exhibition is set to display a diverse range of creative works from artists around the world. From traditional paintings to immersive digital installations, the event aims to celebrate art's ability to communicate across cultures.",
     image: 'https://picsum.photos/200/300',
-    date: '2021-01-04',
+    date: '2022-05-15',
   },
   {
     id: 5,
-    title: 'News 5',
-    description: 'News 5 description',
+    title: "Space Agency's Rover Sends Back Stunning Images from Mars",
+    description:
+      'The latest rover sent to Mars by a space agency has transmitted breathtaking images of the Martian landscape back to Earth. The high-resolution photos provide scientists and space enthusiasts with unprecedented views of the Red Planet.',
     image: 'https://picsum.photos/200/300',
-    date: '2021-01-05',
+    date: '2023-02-28',
   },
   {
     id: 6,
-    title: 'News 6',
-    description: 'News 6 description',
+    title: 'New Book Release: Intriguing Mystery Novel Takes Readers on a Journey',
+    description:
+      'A highly anticipated mystery novel has hit bookstores, promising readers a captivating journey into a world of secrets and suspense. The book has garnered early praise for its intricate plot and well-developed characters.',
     image: 'https://picsum.photos/200/300',
-    date: '2021-01-06',
+    date: '2023-06-20',
   },
   {
     id: 7,
-    title: 'News 7',
-    description: 'News 7 description',
+    title: 'Economic Forum Predicts Shift in Global Trade Patterns',
+    description:
+      'An economic forum comprised of leading economists and policymakers has released a report predicting a significant shift in global trade patterns over the next decade. The findings highlight emerging markets and industries that are expected to drive international commerce.',
     image: 'https://picsum.photos/200/300',
-    date: '2021-01-07',
+    date: '2022-11-10',
   },
 ]
